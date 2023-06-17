242229bfcfdd8f0d10278108fe1ef57d7d7c5c82
jyotirmoycrick <112798691+jyotirmoycrick@users.noreply.github.com>
2023-06-17 13:51:09 +0530

    Add files via upload

---
+++ html tutorial/new.js
@@ -0,0 +1,16 @@
+let a=46;
+
+if(a>=18 && a<=30)
+{
+    console.log("you can drink");
+    
+}
+
+else if(a>=30 && a<=50)
+{
+    console.log("you need to drink pepai");
+}
+
+else{
+    console.log("you can't");
+}
